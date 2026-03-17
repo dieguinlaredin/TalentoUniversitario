18cb1297c610285c9076757e9993260a9a27559f
dieguinlaredin <diegolaredo45@gmail.com>
2026-03-16 20:52:39 -0600

    Update connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,20 +1,21 @@
 import os
 import mysql.connector
-from mysql.connector import Error
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
             host=os.getenv("MYSQLHOST"),
-            port=int(os.getenv("MYSQLPORT", 15913)),
+            port=int(os.getenv("MYSQLPORT", 3306)),
             user=os.getenv("MYSQLUSER"),
             password=os.getenv("MYSQLPASSWORD"),
             database=os.getenv("MYSQLDATABASE"),
             autocommit=True
         )
 
+        print("Conexion exitosa a MySQL")
+
         return connection
 
-    except Error as ex:
+    except Exception as ex:
         print("Error conectando a la base de datos:", ex)
         return None

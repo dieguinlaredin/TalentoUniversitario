fd15d90a7dcaef93e4be575f4ab0bd0e8b5e4368
dieguinlaredin <diegolaredo45@gmail.com>
2026-03-16 20:15:03 -0600

    Update connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -4,12 +4,15 @@ from mysql.connector import Error
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='JazielSQL',
-            database='talento_universitario'
+            host="mysql.railway.internal",
+            user="root",
+            password="yuuiRhHjXpeORsxKmWtwNjIwanAtDPwS",
+            database="railway",
+            port=3306
         )
+
         return connection
+
     except Error as ex:
-        print("Error conectando a la base de datos: ", ex)
+        print("Error conectando a la base de datos:", ex)
         return None
